451e165446e02b9fb4a0f33076dc3d1e010175c9
Icelogia <35977372+Icelogia@users.noreply.github.com>
2025-12-30 22:56:34 +0100

    Debugging effectors and passing effector owner to modifications

--- Assets/Scripts/Ability System/Attributes/AttributeSet.Modifications.cs
+++ Assets/Scripts/Ability System/Attributes/AttributeSet.Modifications.cs
@@ -1,14 +1,30 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace ShatteredIceStudio.AbilitySystem.Attributes
 {
     using Modificators;
     using Effectors;
+    
 
     public partial class AttributeSet
     {
         private CancellationTokenSource cancellationTokenSource;
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// List containing history of applied effectors.
+        /// </summary>
+        public readonly List<EffectorContext> EffectorsHistory = new List<EffectorContext>();
+
+        private const int historyThreshold = 10;
+
+        /// <summary>
+        /// List containg all currently active effectors.
+        /// </summary>
+        public readonly List<EffectorContext> ActiveEffectors = new List<EffectorContext>();
+#endif
 
         protected void InitCancelationToken()
         {
@@ -65,19 +81,55 @@ namespace ShatteredIceStudio.AbilitySystem.Attributes
 
         protected void ApplyInstant(Effector effector, AttributeSet owner)
         {
+#if UNITY_EDITOR
+            EffectorContext context = new EffectorContext
+            {
+                Effector = effector,
+                Owner = owner
+            };
+
+            AddToHistory(context);
+#endif
+
             ApplyTags(effector);
             HandleModifications(effector, owner);
         }
 
         protected async UniTask ApplyDelayed(Effector effector, AttributeSet owner)
         {
+#if UNITY_EDITOR
+            EffectorContext context = new EffectorContext
+            {
+                Effector = effector,
+                Owner = owner
+            };
+
+            AddToHistory(context);
+            AddToActiveEffectors(context);
+#endif
+
             await UniTask.WaitForSeconds(effector.Delay, cancellationToken: cancellationTokenSource.Token);
             ApplyTags(effector);
             HandleModifications(effector, owner);
+
+#if UNITY_EDITOR
+            RemoveFromActiveEffectors(context);
+#endif
         }
 
         protected async UniTask ApplyPeriod(Effector effector, AttributeSet owner)
         {
+#if UNITY_EDITOR
+            EffectorContext context = new EffectorContext
+            {
+                Effector = effector,
+                Owner = owner
+            };
+
+            AddToHistory(context);
+            AddToActiveEffectors(context);
+#endif
+
             ApplyTags(effector);
 
             for (int x = 0; x < effector.Ticks; x++)
@@ -87,6 +139,10 @@ namespace ShatteredIceStudio.AbilitySystem.Attributes
             }
 
             RemoveTags(effector);
+
+#if UNITY_EDITOR
+            RemoveFromActiveEffectors(context);
+#endif
         }
 
         protected virtual void HandleModifications(Effector effector, AttributeSet owner)
@@ -96,5 +152,25 @@ namespace ShatteredIceStudio.AbilitySystem.Attributes
                 mod.ApplyModification(this, owner);
             }
         }
+
+#if UNITY_EDITOR
+        private void AddToHistory(EffectorContext context)
+        {
+            if (EffectorsHistory.Count >= historyThreshold)
+                EffectorsHistory.RemoveAt(0);
+
+            EffectorsHistory.Add(context);
+        }
+
+        private void AddToActiveEffectors(EffectorContext context)
+        {
+            ActiveEffectors.Add(context);
+        }
+
+        private void RemoveFromActiveEffectors(EffectorContext context)
+        {
+            ActiveEffectors.Remove(context);
+        }
+#endif
     }
 }
